78660b56e7121b0c716bc992bb3aed24aa82e9c5
Gajethegejo <gerald.hendryanto@binus.ac.id>
2024-12-04 00:30:21 +0700

    Sync model with database schema, as well as adjusting the schema by adding the address as part of the contacts table, lastly fixing anomalies for phones database, backend, and frontend

--- api/models/phone.model.js
+++ api/models/phone.model.js
@@ -5,9 +5,6 @@ module.exports = (sequelize, Sequelize) => {
             autoIncrement: true,
             primaryKey: true,
         },
-        name: {
-            type: Sequelize.STRING
-        },
         phone_number: {
             type: Sequelize.STRING
         },
